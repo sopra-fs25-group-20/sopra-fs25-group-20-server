a9ead4c7e51029dbfe72bbc9b5823a8e38b9cc6e
Agravlin <ceyhunemrebey@hotmail.com>
2025-03-18 17:09:13 +0100

    chore: test

--- src/main/java/ch/uzh/ifi/hase/soprafs24/controller/UserController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/controller/UserController.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * Test
  * User Controller
  * This class is responsible for handling all REST request that are related to
  * the user.
