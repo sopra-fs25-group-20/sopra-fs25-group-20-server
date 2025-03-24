6dace200762932521039117f5377ae351ce06344
Agravlin <ceyhunemrebey@hotmail.com>
2025-03-24 22:20:14 +0100

    chore: remove template code and adjust project structure

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs25/Application.java
@@ -0,0 +1,23 @@
+package ch.uzh.ifi.hase.soprafs25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @GetMapping(value = "/", produces = MediaType.TEXT_PLAIN_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public String helloWorld() {
+        return "The application is running.";
+    }
+}
