8bf10eb58b7cf60d0328220a890143161ca47861
Agravlin <ceyhunemrebey@hotmail.com>
2025-03-27 00:02:08 +0100

    feat: implement chat message handling via WebSocket

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs25/controller/ChatController.java
@@ -0,0 +1,36 @@
+package ch.uzh.ifi.hase.soprafs25.controller;
+
+import ch.uzh.ifi.hase.soprafs25.model.ChatMessage;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+import java.util.Map;
+
+@Controller
+public class ChatController {
+
+    private final SimpMessagingTemplate messagingTemplate;
+
+    public ChatController(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
+
+    @MessageMapping("/chat")
+    public void createMessage(@Payload ChatMessage message,
+                              Message<?> socketMessage) {
+
+        Map<String, Object> sessionAttributes =
+                (Map<String, Object>) socketMessage.getHeaders().get("simpSessionAttributes");
+
+        String nickname = (String) sessionAttributes.get("nickname");
+        String code = (String) sessionAttributes.get("code");
+        String color = (String) sessionAttributes.get("color");
+
+        message.setNickname(nickname);
+        message.setColor(color);
+        messagingTemplate.convertAndSend("/topic/chat/" + code, message);
+    }
+}
