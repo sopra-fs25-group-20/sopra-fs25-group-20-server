cc1e27edcf1b6a8feb70b8b4cd7896b8ab90ed2f
Agravlin <ceyhunemrebey@hotmail.com>
2025-03-26 23:46:04 +0100

    feat: create ChatMessage object for WebSocket chat messages

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs25/model/ChatMessage.java
@@ -0,0 +1,35 @@
+package ch.uzh.ifi.hase.soprafs25.model;
+
+public class ChatMessage {
+
+    private String nickname;
+    private String message;
+    private String color;
+
+    public ChatMessage() {
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
