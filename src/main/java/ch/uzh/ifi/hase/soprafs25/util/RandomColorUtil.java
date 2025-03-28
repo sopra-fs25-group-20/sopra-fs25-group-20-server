d68c794406c346bc6fdfc401d1a9aef7a7cab853
MischaJampen <mischa.jampen@gmail.com>
2025-03-28 17:10:17 +0100

    feat: add Color palette and getter method in RandomColorUtil

--- src/main/java/ch/uzh/ifi/hase/soprafs25/util/RandomColorUtil.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs25/util/RandomColorUtil.java
@@ -1,9 +1,32 @@
 package ch.uzh.ifi.hase.soprafs25.util;
 
+import java.util.*;
+
 public class RandomColorUtil {
 
-    public static String getRandomColor() {
-        // ToDo
-        return "Green";
+    private static final List<String> COLOR_PALETTE = List.of(
+        "#FF6B6B", // Red
+        "#6BCB77", // Green
+        "#4D96FF", // Blue
+        "#FFD93D", // Yellow
+        "#845EC2", // Purple
+        "#FF9671", // Orange
+        "#00C9A7", // Teal
+        "#C34A36", // Brown
+        "#FFC75F", // Light Orange
+        "#008F7A" // Dark Teal
+    );
+
+    public static String getAvailableColor(Set<String> usedColors) {
+        for (String color : COLOR_PALETTE) {
+            if (!usedColors.contains(color)) {
+                return color;
+            }
+        }
+        throw new IllegalStateException("No available colors left in this room.");
+    }
+
+    public static List<String> getColorPalette() {
+        return COLOR_PALETTE;
     }
 }
