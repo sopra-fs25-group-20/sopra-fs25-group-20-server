333a95e98fe76d389f5321925f7670730e3147df
MischaJampen <mischa.jampen@gmail.com>
2025-03-29 19:50:25 +0100

    feat: add ImageControllerTest

---
+++ src/test/java/ch/uzh/ifi/hase/soprafs25/controller/ImageControllerTest.java
@@ -0,0 +1,23 @@
+package ch.uzh.ifi.hase.soprafs25.controller;
+
+import ch.uzh.ifi.hase.soprafs25.service.image.ImageService;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ImageControllerTest {
+
+    @Test
+    public void testGetImageReturnsBytes() {
+        ImageService mockService = mock(ImageService.class);
+        byte[] fakeImage = new byte[]{1, 2, 3};
+        when(mockService.fetchImage(anyDouble(), anyDouble())).thenReturn(fakeImage);
+
+        ImageController controller = new ImageController(mockService);
+        byte[] response = controller.getImage();
+
+        assertArrayEquals(fakeImage, response);
+        verify(mockService).fetchImage(anyDouble(), anyDouble());
+    }
+}
