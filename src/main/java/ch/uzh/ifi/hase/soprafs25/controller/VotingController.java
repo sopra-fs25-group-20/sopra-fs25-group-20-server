7bd153758153664d95a5c6cae3cdf28ee26843bc
MischaJampen <mischa.jampen@gmail.com>
2025-04-01 20:03:24 +0200

    refactor: refactored castVote and startVote in VotingController to adapt to new Voting entity and VotingService

--- src/main/java/ch/uzh/ifi/hase/soprafs25/controller/VotingController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs25/controller/VotingController.java
@@ -6,6 +6,7 @@ import ch.uzh.ifi.hase.soprafs25.model.VoteCastDTO;
 import ch.uzh.ifi.hase.soprafs25.model.VoteResultDTO;
 import ch.uzh.ifi.hase.soprafs25.repository.RoomRepository;
 import ch.uzh.ifi.hase.soprafs25.service.VotingService;
+import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -29,28 +30,39 @@ public class VotingController {
     }
 
     @MessageMapping("/vote/start")
-    public void startVote(@Payload VoteStartDTO startDTO) {
-        votingService.startVote(startDTO.getRoomCode(), startDTO.getInitiator(), startDTO.getTarget());
-        messagingTemplate.convertAndSend("/topic/vote/update/" + startDTO.getRoomCode(), startDTO);
+    public void startVote(@Payload VoteStartDTO startDTO, Message<?> socketMessage) {
+        Map<String, Object> session = (Map<String, Object>) socketMessage.getHeaders().get("simpSessionAttributes");
+        String roomCode = (String) session.get("code");
+
+        if (!votingService.isVoteSessionActive(roomCode)) {
+            votingService.startVote(roomCode, startDTO.getInitiator(), startDTO.getTarget());
+            messagingTemplate.convertAndSend("/topic/vote/update/" + roomCode, startDTO);
+        }
     }
 
     @MessageMapping("/vote/cast")
-    public void castVote(@Payload VoteCastDTO castDTO) {
-        votingService.castVote(castDTO.getRoomCode(), castDTO.getVoter(), castDTO.isVoteYes());
-        Map<String, Boolean> votes = votingService.getVotes(castDTO.getRoomCode());
-        messagingTemplate.convertAndSend("/topic/vote/update/" + castDTO.getRoomCode(), votes);
+    public void castVote(@Payload VoteCastDTO castDTO, Message<?> socketMessage) {
+        Map<String, Object> session = (Map<String, Object>) socketMessage.getHeaders().get("simpSessionAttributes");
+        String roomCode = (String) session.get("code");
 
-        Room room = roomRepository.findByCode(castDTO.getRoomCode());
+        if (!votingService.hasVoted(roomCode, castDTO.getVoter())) {
+            votingService.castVote(roomCode, castDTO.getVoter(), castDTO.isVoteYes());
+        }
+
+        Map<String, Boolean> votes = votingService.getVotes(roomCode);
+        messagingTemplate.convertAndSend("/topic/vote/update/" + roomCode, votes);
+
+        Room room = roomRepository.findByCode(roomCode);
 
         if (room != null && votes.size() == room.getPlayers().size()) {
             VoteResultDTO result = new VoteResultDTO();
             
-            result.setInitiator(votingService.getInitiator(castDTO.getRoomCode()));
-            result.setTarget(votingService.getTarget(castDTO.getRoomCode()));
+            result.setInitiator(votingService.getInitiator(roomCode));
+            result.setTarget(votingService.getTarget(roomCode));
             result.setVotes(votes);
 
-            messagingTemplate.convertAndSend("/topic/vote/result/" + castDTO.getRoomCode(), result);
-            votingService.clearVote(castDTO.getRoomCode());
+            messagingTemplate.convertAndSend("/topic/vote/result/" + roomCode, result);
+            votingService.endVote(roomCode);
         }
     }
 }
