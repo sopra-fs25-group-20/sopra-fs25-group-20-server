3266286aa866fc48bc4b03eb23d58bfc9328bd97
Osman Öztürk <101147371+osmanoeztuerk@users.noreply.github.com>
2025-04-03 19:08:38 +0200

    feat: controller and services for fetching random images from google api

--- src/main/java/ch/uzh/ifi/hase/soprafs25/controller/ImageController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs25/controller/ImageController.java
@@ -7,7 +7,6 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.concurrent.ThreadLocalRandom;
 
 @RestController
 public class ImageController {
@@ -15,16 +14,14 @@ public class ImageController {
     private final ImageService imageService;
 
     // Change mock to google to use real API
-    public ImageController(@Qualifier("mockImageService") ImageService imageService) {
+    public ImageController(@Qualifier("googleImageService") ImageService imageService) {
         this.imageService = imageService;
     }
 
     @GetMapping(value = "/image", produces = MediaType.IMAGE_JPEG_VALUE)
     @ResponseStatus(HttpStatus.OK)
     public byte[] getImage() {
-        double lat = ThreadLocalRandom.current().nextDouble(-90.0, 90.0); //NOSONAR
-        double lng = ThreadLocalRandom.current().nextDouble(-180.0, 180.0); //NOSONAR
-        return imageService.fetchImage(lat, lng);
+        return imageService.fetchImage();
     }
 
 }
--- src/main/java/ch/uzh/ifi/hase/soprafs25/service/image/GoogleImageService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs25/service/image/GoogleImageService.java
@@ -1,11 +1,37 @@
 package ch.uzh.ifi.hase.soprafs25.service.image;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
 
 @Service("googleImageService")
 public class GoogleImageService implements ImageService {
 
-    @Override
-    public byte[] fetchImage(double lat, double lng) {
-        throw new UnsupportedOperationException("Not implemented yet");
+    private static final String IMAGE_API_URL = "https://maps.googleapis.com/maps/api/streetview";
+    private static final int MAX_ATTEMPTS = 38;
+
+    private final StreetViewMetadataService metadataService;
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    @Value("${google.maps.api.key}")
+    private String apiKey;
+
+    public GoogleImageService(StreetViewMetadataService metadataService) {
+        this.metadataService = metadataService;
+    }
+
+  ImageLoadingException(new Throwable("No StreetView image available after multiple attempts."));
+    }
+
+    private byte[] fetchStreetViewImage(double lat, double lng) {
+        String url = UriComponentsBuilder.fromHttpUrl(IMAGE_API_URL)
+                .queryParam("size", "400x400")
+                .queryParam("location", lat + "," + lng)
+                .queryParam("key", apiKey)
+                .toUriString();
+
+        return restTemplate.getForObject(url, byte[].class);
     }
 }
