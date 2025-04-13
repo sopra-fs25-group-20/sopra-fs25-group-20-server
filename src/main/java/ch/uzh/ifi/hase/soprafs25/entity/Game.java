4c816645906e44f8942d96b345ed8c03c1310a86
Agravlin <ceyhunemrebey@hotmail.com>
2025-04-13 19:28:05 +0200

    feat: adjust game object, add GameResult and GameSettings attributes

--- src/main/java/ch/uzh/ifi/hase/soprafs25/entity/Game.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs25/entity/Game.java
@@ -3,6 +3,8 @@ package ch.uzh.ifi.hase.soprafs25.entity;
 import ch.uzh.ifi.hase.soprafs25.constant.GamePhase;
 import ch.uzh.ifi.hase.soprafs25.constant.PlayerRole;
 import ch.uzh.ifi.hase.soprafs25.util.RoleUtil;
+
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.List;
@@ -11,14 +13,48 @@ public class Game {
     
     private final String roomCode;
     private GamePhase phase;
-    private int highlightedImage;
+    private int highlightedImageIndex;
     private final Map<String, PlayerRole> roles = new HashMap<>();
-    private final int spyCount;
+    private final GameSettings gameSettings;
+    private GameResult gameResult;
+    private final List<byte[]> images = new ArrayList<>();
 
     public Game(String roomCode) {
         this.roomCode = roomCode;
         this.phase = GamePhase.LOBBY;
-        this.spyCount = 1; // default for now
+        this.gameSettings = new GameSettings(1, 30, 300, 9, "europe");
+    }
+
+    public PlayerRole getRole(String nickname) {
+        return roles.get(nickname);
+    }
+
+    public GameResult getGameResult() {
+        return gameResult;
+    }
+
+    public void setGameResult(int spyGuessIndex, String votedNickname, PlayerRole winnerRole) {
+        this.gameResult = new GameResult(spyGuessIndex, votedNickname, winnerRole);
+    }
+
+    public GameSettings getGameSettings() {
+        return gameSettings;
+    }
+
+    public void setGameSettings(int votingTimer, int gameTimer, String imageRegion, int imageCount) {
+        this.gameSettings.setVotingTimer(votingTimer);
+        this.gameSettings.setGameTimer(gameTimer);
+        this.gameSettings.setImageRegion(imageRegion);
+        this.gameSettings.setImageCount(imageCount);
+    }
+
+    public Map<String, PlayerRole> getRoles() {
+        return roles;
+    }
+
+    public void assignRoles(List<String> nicknames) {
+        roles.clear();
+        roles.putAll(RoleUtil.assignRoles(nicknames, gameSettings.getSpyCount()));
     }
 
     public String getRoomCode() {
@@ -33,24 +69,20 @@ public class Game {
         this.phase = phase;
     }
 
-    public int getHighlightedImage() {
-        return highlightedImage;
+    public int getHighlightedImageIndex() {
+        return highlightedImageIndex;
     }
 
-    public void setHighlightedImage(int highlightedImage) {
-        this.highlightedImage = highlightedImage;
+    public void setHighlightedImageIndex(int highlightedImageIndex) {
+        this.highlightedImageIndex = highlightedImageIndex;
     }
 
-    public Map<String, PlayerRole> getRoles() {
-        return roles;
+    public List<byte[]> getImages() {
+        return images;
     }
 
-    public void assignRoles(List<String> nicknames) {
-        roles.clear();
-        roles.putAll(RoleUtil.assignRoles(nicknames, spyCount));
-    }
-
-    public PlayerRole getRole(String nickname) {
-        return roles.get(nickname);
+    public void setImages(List<byte[]> newImages) {
+        images.clear();
+        images.addAll(newImages);
     }
 }
---
+++ src/main/java/ch/uzh/ifi/hase/soprafs25/entity/GameResult.java
@@ -0,0 +1,31 @@
+package ch.uzh.ifi.hase.soprafs25.entity;
+
+import ch.uzh.ifi.hase.soprafs25.constant.PlayerRole;
+
+public class GameResult {
+    private int spyGuessIndex;
+    private String votedNickname;
+    private PlayerRole winnerRole;
+
+    public GameResult(int spyGuessIndex, String votedNickname, PlayerRole winnerRole) {
+        this.spyGuessIndex = spyGuessIndex;
+        this.votedNickname = votedNickname;
+        this.winnerRole = winnerRole;
+    }
+
+    public int getSpyGuessIndex() {
+        return spyGuessIndex;
+    }
+
+    public String getVotedNickname() {
+        return votedNickname;
+    }
+
+    public PlayerRole getWinnerRole() {
+        return winnerRole;
+    }
+
+    public void setWinnerRole(PlayerRole winnerRole) {
+        this.winnerRole = winnerRole;
+    }
+}
