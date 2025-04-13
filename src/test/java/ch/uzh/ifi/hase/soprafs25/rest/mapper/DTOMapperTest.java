90b9a2b7090db19fee23d607a7a02821c63a0edf
Agravlin <ceyhunemrebey@hotmail.com>
2025-04-13 12:03:59 +0200

    fix: correct the package path

--- src/test/java/ch/uzh/ifi/hase/soprafs25/rest/mapper/DTOMapperTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs25/rest/mapper/DTOMapperTest.java
@@ -1,5 +1,9 @@
-package ch.uzh.ifi.hase.soprafs25.model;
+package ch.uzh.ifi.hase.soprafs25.rest.mapper;
 
+import ch.uzh.ifi.hase.soprafs25.model.ChatMessageDTO;
+import ch.uzh.ifi.hase.soprafs25.model.CreateRoomDTO;
+import ch.uzh.ifi.hase.soprafs25.model.ErrorResponseDTO;
+import ch.uzh.ifi.hase.soprafs25.model.RoomPostDTO;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
