7f7eb43cabec9fbe395ec019189e036e97704a93
Agravlin <ceyhunemrebey@hotmail.com>
2025-04-15 22:21:02 +0200

    fix: replace int with Integer to allow null in GameResultDTO for spyGuessIndex attribute

--- src/main/java/ch/uzh/ifi/hase/soprafs25/model/GameResultDTO.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs25/model/GameResultDTO.java
@@ -29,7 +29,7 @@ public class GameResultDTO {
         return highlightedImageIndex;
     }
 
-    public int getSpyGuessIndex() {
+    public Integer getSpyGuessIndex() {
         return spyGuessIndex;
     }
 
