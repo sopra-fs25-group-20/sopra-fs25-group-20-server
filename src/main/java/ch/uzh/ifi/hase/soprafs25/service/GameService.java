6da638dea129779d684080bdf773577f1b8917d9
Agravlin <ceyhunemrebey@hotmail.com>
2025-04-15 16:58:21 +0200

    feat: broadcast game phase after spy guesses an image

--- src/main/java/ch/uzh/ifi/hase/soprafs25/service/GameService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs25/service/GameService.java
@@ -58,6 +58,7 @@ public class GameService {
         }
 
         advancePhase(roomCode, GamePhase.SUMMARY);
+        gameBroadcastService.broadcastGamePhase(roomCode);
     }
 
     public void changeGameSettings(String roomCode, String nickname, GameSettingsDTO gameSettings) {
