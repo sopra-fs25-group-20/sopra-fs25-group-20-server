a0db0479489097f264816b368d5689b23723948f
Agravlin <ceyhunemrebey@hotmail.com>
2025-05-12 17:31:00 +0200

    feat: associate user account with token on room creation

--- src/main/java/ch/uzh/ifi/hase/soprafs25/service/CreateRoomService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs25/service/CreateRoomService.java
@@ -2,7 +2,10 @@ package ch.uzh.ifi.hase.soprafs25.service;
 
 import ch.uzh.ifi.hase.soprafs25.entity.Player;
 import ch.uzh.ifi.hase.soprafs25.entity.Room;
+import ch.uzh.ifi.hase.soprafs25.entity.User;
+import ch.uzh.ifi.hase.soprafs25.exceptions.UserNotAuthenticatedException;
 import ch.uzh.ifi.hase.soprafs25.repository.RoomRepository;
+import ch.uzh.ifi.hase.soprafs25.repository.UserRepository;
 import ch.uzh.ifi.hase.soprafs25.util.RoomCodeUtil;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,19 +17,32 @@ public class CreateRoomService {
     private final RoomRepository roomRepository;
     private final JoinRoomService joinRoomService;
     private final GameService gameService;
+    private final UserRepository userRepository;
 
     public CreateRoomService(RoomRepository roomRepository,
-                             JoinRoomService joinRoomService, GameService gameService) {
+                             JoinRoomService joinRoomService,
+                             GameService gameService,
+                             UserRepository userRepository) {
         this.roomRepository = roomRepository;
         this.joinRoomService = joinRoomService;
         this.gameService = gameService;
+        this.userRepository = userRepository;
     }
 
 
-    public Player createRoom(Player player) {
+    public Player createRoom(Player player, String tokenHeader) {
         Room createdRoom = new Room();
         createdRoom.setCode(RoomCodeUtil.generateRoomCode());
         createdRoom = roomRepository.save(createdRoom);
+
+        if (tokenHeader != null && tokenHeader.startsWith("Bearer ")) {
+            String token = tokenHeader.substring(7);
+            User user = userRepository.findByToken(token);
+            if (user == null) {
+                throw new UserNotAuthenticatedException("Invalid token. Please log in again.");
+            }
+            player.setUser(user);
+        }
 
         Player savedPlayer = joinRoomService.joinRoom(createdRoom.getCode(), player);
 
